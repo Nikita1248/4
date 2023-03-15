cba1f5fecaa4d37e21080b99f9f103b4b65d6c29
Nikita <93786750+Nikita1248@users.noreply.github.com>
2023-03-15 20:33:58 +0300

    Add files via upload

---
+++ Player.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Player : MonoBehaviour
+{
+    private int coins;
+    private int health = 10;
+
+    public GameObject fireballPrefab;
+    public Transform attackPoint;
+
+    public AudioSource audioSource;
+    public AudioClip damageSound;
+
+    public void CollectCoins(){
+        coins++;
+
+
+        print("Собранные монетки:" + coins);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+
+        if (health > 0)
+        {
+            audioSource.PlayOneShot(damageSound);
+        }
+        else
+        {
+            int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(sceneIndex);
+        }
+        print("Здоровье игрока: " + health);
+    }
+
+      void Update()
+    {
+ 
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(fireballPrefab, attackPoint.position, attackPoint.rotation);
+        }
+ 
+    }
+
+}
